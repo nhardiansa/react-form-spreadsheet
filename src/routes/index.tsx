ccf366639b09dea0ccbc970784b67d26b31f2153
Nabil Hardiansa <nhardiansa@gmail.com>
2025-11-22 16:48:43 +0800

    feat: enhance routing by adding Link component for navigation and update Vercel rewrites

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
@@ -8,7 +8,7 @@ function RouteComponent() {
   return (
     <div className='flex flex-col items-center justify-center h-screen gap-4'>
       <h1 className='font-bold text-3xl'>
-        Silahkan kunjungi <code className='bg-slate-200 px-2 py-1 rounded-md'>/iltizamat</code> untuk melihat form komitmen.
+        Silahkan kunjungi <code className='bg-slate-200 px-2 py-1 rounded-md'><Link to='/iltizamat'>/iltizamat</Link></code> untuk melihat form komitmen.
       </h1>
     </div>)
 }
